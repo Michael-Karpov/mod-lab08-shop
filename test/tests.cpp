8338471eb30e83012398ae0aad7ea9d5e09e40f3
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-05-15 13:11:22 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,23 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "../include/task.h"
+
+TEST(task1, test1) {
+	Shop shop(2, 30, 200);
+	int result = shop.stats.served + shop.stats.rejected;
+	EXPECT_EQ(30, result);
+}
+
+TEST(task2, test2) {
+	Shop shop(4, 60, 200);
+	int result = shop.stats.rejected;
+	EXPECT_EQ(0, result);
+}
+
+TEST(task3, test3) {
+	Shop shop(2, 60, 250);
+	int result = 0;
+	if (shop.stats.rejected > 0)
+		result = 1;
+	EXPECT_EQ(1, result);
+}
