9137d8a38a07d46e286ba54d2ed2aef7d5fd30ce
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-05-15 13:22:55 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,21 +3,21 @@
 #include "../include/task.h"
 
 TEST(task1, test1) {
-	Shop shop(2, 30, 200);
+	Shop shop(3, 10, 10);
 	int result = shop.stats.served + shop.stats.rejected;
-	EXPECT_EQ(30, result);
+	EXPECT_EQ(113, result);
 }
 
 TEST(task2, test2) {
-	Shop shop(4, 60, 200);
+	Shop shop(4, 10, 20);
 	int result = shop.stats.rejected;
-	EXPECT_EQ(0, result);
+	EXPECT_EQ(81, result);
 }
 
 TEST(task3, test3) {
-	Shop shop(2, 60, 250);
+	Shop shop(1, 4, 100);
 	int result = 0;
-	if (shop.stats.rejected > 0)
+	if (shop.stats.rejected > 100)
 		result = 1;
 	EXPECT_EQ(1, result);
 }
