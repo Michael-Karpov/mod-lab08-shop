23b2f3d0757eb4e860e1914fbf59d2fd17acb2f8
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-05-16 15:32:08 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,28 +2,26 @@
 #include <gtest/gtest.h>
 #include "../include/task.h"
 
-TEST(TEST1, test1) {
-	Shop shop(3, 10, 10);
-	shop.Start();
-	shop.Stop();
-	int result = shop.stats.served + shop.stats.rejected;
-	EXPECT_EQ(113, result);
+
+TEST(task1, test1) {
+	Shop shop(1, 10, 100, 1, 1);
+	shop.Shopping();
+	int result = shop.servedClients;
+	EXPECT_EQ(10, result);
 }
 
-TEST(TEST2, test2) {
-	Shop shop(4, 10, 20);
-	shop.Start();
-	shop.Stop();
-	int result = shop.stats.rejected;
-	EXPECT_EQ(81, result);
+TEST(task2, test2) {
+	Shop shop(8, 92, 300, 5, 8);
+	shop.Shopping();
+	int result = shop.rejectedClients;
+	EXPECT_EQ(0, result);
 }
 
-TEST(TEST3, test3) {
-	Shop shop(1, 4, 100);
-	shop.Start();
-	shop.Stop();
+TEST(task3, test3) {
+	Shop shop(1, 60, 228, 5, 2);
+	shop.Shopping();
 	int result = 0;
-	if (shop.stats.rejected > 100)
+	if (shop.rejectedClients > 30)
 		result = 1;
 	EXPECT_EQ(1, result);
 }
